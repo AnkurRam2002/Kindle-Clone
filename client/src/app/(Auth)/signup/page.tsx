726d89ada1e769c7a109b0e938eec2ebb8345ab6
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-27 20:45:05 +0530

    resolved minor bugs

--- client/src/app/(Auth)/signup/page.tsx
+++ client/src/app/(Auth)/signup/page.tsx
@@ -38,7 +38,7 @@ const handleSubmit = async (e:any) => {
 
 
       try {
-        const response = await fetch('http://localhost:5000/api/auth/signup', {
+        const response = await fetch(apiurl + 'api/auth/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
