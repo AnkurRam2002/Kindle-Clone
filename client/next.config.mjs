480abbad445432c8e2ab902a4bbdd082822b55ea
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-28 16:32:58 +0530

    some changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -7,9 +7,12 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: "images.pexels.com"
+          },
+          {
+            protocol: 'https',
+            hostname: "m.media-amazon.com"
           }
         ],
-        domains: ['m.media-amazon.com'],
     }
       
 };
